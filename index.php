b810e243c3431e568ecb7fc17e90bd3600484150
KraskovskiiNA <58112849+KraskovskiiNA@users.noreply.github.com>
2020-10-09 21:05:38 +0300

    1.2

--- index.php
+++ index.php
@@ -2,25 +2,58 @@
 
 // Ex 1
 
-function bracket($value) : bool
-{
-    $stack = [];
+// function bracket($value) : bool
+// {
+//     $stack = [];
 
-    for ($i = 0; $i < strlen($value); $i++) {
-        
-        if ($value[$i] == ')' || $value[$i] == '}' || $value[$i] == ']') {
-            $lastElem = array_pop($stack);
-            if ($value[$i] == ')' && $lastElem !== '(' || $value[$i] === '}' && $lastElem !== '{' || $value[$i] == ']' && $lastElem !== '[') 
-                { return false; }
-        } else {
-                $stack[] = $value[$i];
+//     for ($i = 0; $i < strlen($value); $i++) {
+//         if ($value[$i] == ')' || $value[$i] == '}' || $value[$i] == ']') {
+//             $lastElem = array_pop($stack);
+//             if ($value[$i] == ')' && $lastElem !== '(' || $value[$i] === '}' && $lastElem !== '{' || $value[$i] == ']' && $lastElem !== '[') 
+//                 { return false; }
+//         } else {
+//                 $stack[] = $value[$i];
+//             }
+//     }
+//     return true;
+// }
+// $res = bracket('([{}])');  // true
+
+
+$values = ['((a + b)/ c) - 2', '"([ошибка)"', '"(")', '{[][()]}'];
+$result = [];
+$test = [')' => '(', ']' => '[', '}' => '{'];
+
+foreach ($values as $value) {
+    $stack = new SplStack();
+    foreach (str_split($value) as $str) {
+        if ($stack->valid() && $stack->top() == '"') {
+            if ($str == '"') {
+                $stack->pop();
             }
+            continue;
+        }
+        if ($stack == '"') {
+            $stack->push($str);
+            continue;
+        }
+        if (in_array($str, $test)) {
+            $stack->push($str);
+        }
+        if (key_exists($str, $test)) {
+            if ($test[$str] != $stack->top()) {
+                break;
+            }
+        $stack->pop();
+        }
     }
-    return true;
+    $result[] = $stack->isEmpty();
 }
 
-$res = bracket('([{}])');  // true
-$res1 = bracket('([)');  // false
+for ($i = 0; $i < count($values); $i++) {
+    echo $values[$i] . '=>' . ($result[$i] ? 'True' : 'False') . '<br>';
+}
+
 
 //  Ex 2
 
@@ -41,13 +74,44 @@ function maxInt($value)
 $end = microtime(true);
 $result = maxInt(600851475143); // 6857
 echo $result . '<br>';  
-echo  $start - $end;
+
 
 // Ex 3
 
-// $dir = new DirectoryIterator('/');
+$path = empty($_GET["dir"]) ? 'C:\\' : $_GET["dir"];
+$dirs = [];
+$files =[];
 
-// foreach ($dir as $items) {
-//     echo $items . '<br>';
-// }
+foreach (new DirectoryIterator($path) as $item) {
+    if ($item->isDot()) {
+        continue;
+    }
+    if ($item->isDir()) {
+        $dirs[] = $item->getFilename();
+    } else {
+        $files[] = $item->getFilename();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if (dirname($path) != $path) : ?>
+        <a href="?">Up<?php dirname($path) ?></a><br>
+    <?php endif; ?>
+    <?php foreach ($dirs as $dir) : ?>
+        <a href="?dir=<?= $path . '/' . $dir ?>"><?= $dir ?></a><br>
+    <?php endforeach; ?>
+    <?php foreach ($files as $file) : ?>
+        <p><?= $file ?></p><br>
+    <?php endforeach; ?>
+</body>
+</html>
 
